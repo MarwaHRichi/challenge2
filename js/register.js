f9419dd58b65613deea919a62733f70ec240aacd
MarwaHRichi <marwahrichi93@gmail.com>
2021-11-23 11:17:30 +0100

    commit

--- js/register.js
+++ js/register.js
@@ -1,4 +1,12 @@
 
-function register() {
-    
+var validation = document.getElementById("inscrire")
+var valid = document.getElementById("nom");
+var message = document.getElementById("invalid");
+validation.addEventListener("click", register);
+function register(e) {
+    if (valid.validity.valueMissing){
+        e.preventDefault();
+        message.textContent = "obligatoir";
+        message.style.color ="red";
+    }
 }
